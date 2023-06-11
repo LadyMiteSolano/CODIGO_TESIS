7ba8e284d196027d43c29adfc0b3e0e6a7c780c3
LadyMiteSolano <56986716+LadyMiteSolano@users.noreply.github.com>
2023-06-11 14:11:44 -0500

    CREA PROYECTO

---
+++ EXAMPLE/src/test/java/EXAMPLE/ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package EXAMPLE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
